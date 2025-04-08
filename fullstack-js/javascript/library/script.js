90a0d8a795d2563a6ba836b0a31079aada62e772
Adzfar <53836459+adzzzfarr@users.noreply.github.com>
2025-04-08 19:13:49 +0800

    Added delete button to cards

--- fullstack-js/javascript/library/script.js
+++ fullstack-js/javascript/library/script.js
@@ -22,41 +22,54 @@ function addBookToLibrary(title, author, pages, isRead, library) {
     library.push(book);
 }
 
+function removeBookFromLibrary(id, library) {
+    const index = library.findIndex(book => book.id === id);
+    library.splice(index, 1);
+}
+
 function displayBooks(container, library) {
     container.innerHTML = '';
 
     library.forEach((book) => {
         const card = document.createElement('div');
         card.classList.add('book-card'); // Apply CSS class
+        
+        // data attribute
+        card.setAttribute('data-bookid', book.id);
 
+        // title element
         const titleElement = document.createElement('h1');
         titleElement.textContent = book.title;
         titleElement.style.fontSize = '1.375rem';
 
+        // author element
         const authorElement = document.createElement('p');
         authorElement.textContent = `Author: ${book.author}`;
 
+        // pages element
         const pagesElement = document.createElement('p');
         pagesElement.textContent = `Pages: ${book.pages}`;
 
+        // read element
         const readElement = document.createElement('p');
         readElement.textContent = `Status: ${book.isRead ? 'Read' : 'Not Read Yet'}`;
+
+        // delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => onClickDelete(card.dataset.bookid, library, container));
 
         card.appendChild(titleElement);
         card.appendChild(authorElement);
         card.appendChild(pagesElement);
         card.appendChild(readElement);
+        card.appendChild(deleteButton);
 
         container.appendChild(card);
     });
 }
 
-
-const submit = document.querySelector('#submit');
-
-submit.addEventListener('click', onClickSubmit);
-
-function onClickSubmit(event) {
+function onClickSubmit(event, container, library) {
     event.preventDefault()
 
     const title = document.querySelector('#title').value;
@@ -66,9 +79,13 @@ function onClickSubmit(event) {
     const completionStatus = document.querySelector('input[name="completion"]:checked');
     const isRead = completionStatus && completionStatus.value == 'read';
 
-    addBookToLibrary(title, author, pages, isRead, myLibrary);
-    console.log(myLibrary[3].title)
-    displayBooks(content, myLibrary);
+    addBookToLibrary(title, author, pages, isRead, library);
+    displayBooks(container, library);
+}
+
+function onClickDelete(id, library, container) {
+    removeBookFromLibrary(id, library);
+    displayBooks(container, library);
 }
 
 const myLibrary = [];
@@ -78,3 +95,6 @@ addBookToLibrary('The Hobbit 3', 'J.R.R Tolkien', 305, false, myLibrary);
 
 const content = document.querySelector("#content");
 displayBooks(content, myLibrary);
+
+const submit = document.querySelector('#submit');
+submit.addEventListener('click', (event) => onClickSubmit(event, content, myLibrary));
