10da17eba7a672a3f60b96b83c8a63523eabac27
Adzfar <53836459+adzzzfarr@users.noreply.github.com>
2025-04-13 19:38:55 +0800

    Working UI for the game

--- fullstack-js/javascript/tic-tac-toe/script.js
+++ fullstack-js/javascript/tic-tac-toe/script.js
@@ -11,12 +11,19 @@ function Gameboard() {
     const columns = 3;
     const board = [];
 
-    // create a 2D array
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(0);
+    // resetBoard method
+    const resetBoard = () => {
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+                board[i].push(0);
+            }
         }
+    }
+
+    // populate board upon instantiation
+    if (board.length === 0) {
+        resetBoard();
     }
 
     // getBoard method
@@ -24,7 +31,16 @@ function Gameboard() {
 
     // addMarker method
     const addMarker = (row, column, marker) => {
-        board[row][column] = marker;
+        if (board[row][column] === 0) {   
+            board[row][column] = marker;
+        } else {
+            alert('Cell already taken!');
+        }
+    }
+
+    // checkDraw method
+    const checkDraw = () => {
+        return board.every((row) => row.every((cell) => cell !== 0));
     }
 
     // checkWin method
@@ -50,15 +66,14 @@ function Gameboard() {
             count += countConsecutive(row, column, -dx, -dy);
 
             if (count === 3) {
-                console.log(true);
+                console.log('Won!');
                 return true;
             }
         }
 
-        console.log(false);
         return false;
 
-        // helper function to count consecutive matching markers, excluding the placed marker itself
+        // count consecutive matching markers, excluding the placed marker itself
         function countConsecutive(startRow, startColumn, dx, dy) {
             count = 0;
             currRow = startRow + dx;
@@ -76,25 +91,90 @@ function Gameboard() {
         }
     }
 
-    // displayBoard method  
-    const displayBoard = () => {
-        const boardSnapshot = board.map(
-            (row) => row.map(
-                (cell) => cell
-            )
-        );
+    const buildBoard = (doc, container, controller) => {
+        container.innerHTML = ''
+        
+        for (let i = 0; i < board.length; i++) {
+            currRow = board[i];
 
-        console.log(boardSnapshot);
+            const rowElement = doc.createElement('div');
+            rowElement.classList.add('row');
+
+            for (let j = 0; j < currRow.length; j++) {
+                currCell = currRow[j];
+
+                const cellElement = buildCellElement(currCell);
+
+                cellElement.addEventListener('mouseover', () => {
+                    cellElement.style.backgroundColor = 'gainsboro';
+                });
+    
+                cellElement.addEventListener('mouseleave', () => {
+                    cellElement.style.backgroundColor = 'white';
+                });
+                
+                cellElement.addEventListener('click', () => {
+                    controller.playRound(i, j);
+                    buildBoard(doc, container, controller);
+                });
+
+                rowElement.appendChild(cellElement);
+            }
+
+            container.appendChild(rowElement);
+        }
+
+        function buildCellElement(cell) {
+            const cellElement = doc.createElement('div');
+            cellElement.classList.add('cell');
+            cellElement.textContent = cell === 0 ? '' : cell;
+
+            return cellElement;
+        }
     }
 
-    return { getBoard, addMarker, checkWin, displayBoard };
+    return { resetBoard, getBoard, addMarker, checkDraw, checkWin, buildBoard };
+}   
+
+function GameController(board, playerOneName = 'Player One', playerTwoName = 'Player Two') {
+    const players = [
+        new Player(playerOneName, 'X'),
+        new Player(playerTwoName, 'O')
+    ];
+
+    let activePlayer = players[0];
+
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+
+    const playRound = (row, column) => {
+        if (board.getBoard()[row][column] !== 0) {
+            alert('Cell already taken!');
+            return;
+        } else {
+            board.addMarker(row, column, activePlayer.token);
+        }
+
+        if (board.checkDraw()) {
+            alert('Draw!');
+        } else if (board.checkWin(row, column, activePlayer.token)) {
+            alert(`${activePlayer.name} won!`);
+        } else {
+            switchPlayer();
+        }
+    }
+
+    return { getActivePlayer, playRound }
 }   
 
 
-const board = Gameboard();
-board.displayBoard();
-board.addMarker(0, 0, 1);    
-board.addMarker(1, 1, 1);
-board.addMarker(2, 1, 1);
-board.displayBoard();    
-board.checkWin(0, 0, 1);   
+const TicTacToe = (function(doc) {
+    const boardContainer = doc.querySelector('#board');
+    const board = Gameboard();
+    const controller = GameController(board);
+
+    board.buildBoard(doc, boardContainer, controller);
+})(document);
