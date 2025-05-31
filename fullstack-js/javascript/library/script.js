c5de5c1c4b81787b5470e55204e96fcb6a386b31
Adzfar <53836459+adzzzfarr@users.noreply.github.com>
2025-05-31 19:45:08 +0800

    Added custom validation

--- fullstack-js/javascript/library/script.js
+++ fullstack-js/javascript/library/script.js
@@ -19,16 +19,15 @@ class Book {
 }
 
 class LibraryApp {
-    constructor(contentSelector, submitButtonSelector) {
+    constructor() {
         this.library = [
             new Book('The Hobbit', 'J.R.R Tolkien', 295, false),
             new Book('The Hobbit 2', 'J.R.R Tolkien', 300, false),
             new Book('The Hobbit 3', 'J.R.R Tolkien', 305, false),
         ];
 
-        this.content = document.querySelector(contentSelector);
-        this.submitButton = document.querySelector(submitButtonSelector);
-        this.submitButton.addEventListener('click', (event) => this.onClickSubmit(event));
+        const form = document.querySelector('form');
+        form.addEventListener('submit', (event) => this.onClickSubmit(event));
         
         this.displayBooks();
     }
@@ -51,20 +50,27 @@ class LibraryApp {
 
     onClickSubmit(event) {
         event.preventDefault()
+
+        const isValid = customValidityCheck();
+        if (!isValid) return;
     
-        const title = document.querySelector('#title').value;
-        const author = document.querySelector('#author').value;
-        const pages = document.querySelector('#pages').value;
+        const form = document.querySelector('form');
+        const title = document.querySelector('#title');
+        const author = document.querySelector('#author');
+        const pages = document.querySelector('#pages');
     
         const completionStatus = document.querySelector('input[name="completion"]:checked');
         const isRead = completionStatus && completionStatus.value == 'read';
     
-        this.addBook(title, author, pages, isRead);
+        this.addBook(title.value, author.value, parseInt(pages.value), isRead);
         this.displayBooks();
+        form.reset();  
     }
 
     displayBooks() {
-        this.content.innerHTML = '';
+        const content = document.getElementById('content');
+
+        content.innerHTML = '';
 
         this.library.forEach((book) => {
             const card = document.createElement('div');
@@ -113,11 +119,69 @@ class LibraryApp {
             card.appendChild(deleteButton);
             card.appendChild(toggleReadStatusButton);
 
-            this.content.appendChild(card);
+
+            content.appendChild(card);
         });
     }
 }
 
+// Validation Logic
+function customValidityCheck() {
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const pages = document.querySelector('#pages');
+    const completion = document.querySelector('input[name="completion"]:checked');
+
+    const titleError = document.querySelector("#title + span.error");
+    const authorError = document.querySelector("#author + span.error");
+    const pagesError = document.querySelector("#pages + span.error");
+    const completionError = document.querySelector('#completion-buttons + .error');
+
+    let valid = true;
+
+    if (!title.validity.valid) {
+        titleError.textContent = "You need to enter a title.";
+        titleError.className = "error active";
+        valid = false;
+    } else {
+        titleError.textContent = "";
+        titleError.className = "error";
+    }
+
+    if (!author.validity.valid) {
+        authorError.textContent = "You need to enter an author.";
+        authorError.className = "error active";
+        valid = false;
+    } else {
+        authorError.textContent = "";
+        authorError.className = "error";
+    }
+
+    if (!pages.validity.valid) {
+        if (pages.validity.valueMissing) {
+            pagesError.textContent = "You need to enter the number of pages.";
+        } else if (pages.validity.rangeUnderflow) {
+            pagesError.textContent = "Number of pages must be greater than 0.";
+        }
+        pagesError.className = "error active";
+        valid = false;
+    } else {
+        pagesError.textContent = "";
+        pagesError.className = "error";
+    }
+
+    if (!completion) {
+        completionError.textContent = "You need to specify the completion status.";
+        completionError.className = "error active";
+        valid = false;
+    } else {
+        completionError.textContent = "";
+        completionError.className = "error";
+    }
+
+    return valid;
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    new LibraryApp('#content', '#submit');
-})
+    new LibraryApp();
+});
