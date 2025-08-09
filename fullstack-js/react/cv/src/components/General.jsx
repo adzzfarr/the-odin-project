122c61b8216f1ce7e2ed8be9bfe758807b5b2fa1
Adzfar <md.nur.adzfar@gmail.com>
2025-08-09 14:29:32 +0800

    Undo unnecessary changes

--- fullstack-js/react/cv/src/components/General.jsx
+++ fullstack-js/react/cv/src/components/General.jsx
@@ -1,35 +1,21 @@
 import { useState } from "react";
 
 export function General() {
-    const [inputName, setInputName] = useState('');
-    const handleNameChange = (e) => {
-        setInputName(e.target.value);
-    }
-
-    const [inputEmail, setInputEmail] = useState('');
-    const handleEmailChange = (e) => {
-        setInputEmail(e.target.value);
-    }
-
-    const [inputNumber, setInputNumber] = useState('');
-    const handleNumberChange = (e) => {
-        setInputEmail(e.target.value);
-    }
 
     return (
         <fieldset>
             <legend>General Information</legend>
             <div className="labelInput">
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" value={inputName} onChange={handleNameChange} />
+                <input type="text" id="name" name="name" />
             </div>
             <div className="labelInput">
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" value={inputEmail} onChange={handleEmailChange} />
+                <input type="email" id="email" name="email" />
             </div>
             <div className="labelInput">
                 <label htmlFor="number">Phone Number</label>
-                <input type="tel" id="number" name="number" value={inputNumber} onChange={handleNumberChange} />
+                <input type="tel" id="number" name="number" />
             </div>
         </fieldset>
     );
